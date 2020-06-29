9ac04d1c614855e27cabff8e6930d400f32e8490
Hanane Chrif El Asri <65400951+hanane-ca@users.noreply.github.com>
2020-06-29 16:43:17 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,11 +7,11 @@ public class Main {
 		
 		//encodage
 		String text = "cette phrase est un test";
-		codage.Codage(text,codage.huffman());
+		codage.Codage(text,codage.huffman(text));
 		
 		//décodage
-		String code = "010100010101111101110000111011010111000001110100111001011111101010101101";
-		codage.Decodage(code);
+		String code = "1100001111111100111011111001110100101011000011101100100111111101111010111110111111001001111";
+		codage.Decodage(code,text);
 		
 		
 		//pour génerer le fichier qui nous permet de tracer la courbe de complexité
