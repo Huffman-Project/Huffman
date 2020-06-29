4d33eb3d0122a92fd5904c57a8ca37de1fecfe0c
Hanane Chrif El Asri <65400951+hanane-ca@users.noreply.github.com>
2020-06-29 12:35:16 +0100

    encodage/decodage (complexité en comment)

--- Main.java
+++ Main.java
@@ -3,18 +3,21 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		
-        /*CodageDecod codage = new CodageDecod();
+        	CodageDecod codage = new CodageDecod();
 		
+		//encodage
 		String text = "cette phrase est un test";
 		codage.Codage(text,codage.huffman());
 		
+		//décodage
 		String code = "010100010101111101110000111011010111000001110100111001011111101010101101";
-		codage.Decodage(code);*/
-		
-		Complexity comp = new Complexity();
+		codage.Decodage(code);
 		
 		
-		comp.genere_fichier();
+		//pour génerer le fichier qui nous permet de tracer la courbe de complexité
+		
+		/*Complexity comp = new Complexity();
+		comp.genere_fichier();*/
 		
 	}
 
